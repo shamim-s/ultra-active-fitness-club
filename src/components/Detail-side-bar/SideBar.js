11591a8ccc7b2d16494b3e8ce3239946494091e2
Shamim Sarkar <shamimibas@gmail.com>
2022-09-29 13:45:59 +0600

    Rest time and Exercise time handler added

--- src/components/Detail-side-bar/SideBar.js
+++ src/components/Detail-side-bar/SideBar.js
@@ -8,8 +8,11 @@ const SideBar = ({cart}) => {
     for(const item of cart){
         requiredTime = requiredTime + parseInt(item.required)
     }
-    
-    console.log(requiredTime);
+
+    const restTimeButton = (id) =>{
+      document.getElementById('restTime').innerText = document.getElementById(id).innerText; 
+    }
+
     return (
         <div>
             <div className='flex mt-5 mb-3'>
@@ -19,6 +22,7 @@ const SideBar = ({cart}) => {
                     <h2><span className='text-slate-300'><FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon></span> Dinajpur, Bangladesh.</h2>
                 </div>
             </div>
+
             <div className='flex justify-around items-center bg-slate-900 h-20 rounded-lg'>
                 <div className='text-white'>
                     <h1 className='text-2xl'>65<span className='text-base'>kg</span></h1>
@@ -33,28 +37,32 @@ const SideBar = ({cart}) => {
                     <p>Age</p>
                 </div>
             </div>
+
             <h1 className='text-2xl text-slate-900 font-bold mt-5'>Add A Break</h1>
+
             <div className='flex justify-around items-center mt-5 bg-slate-200 h-20 rounded-lg'>
-                <div className='bg-slate-400 hover:bg-slate-900 hover:text-white font-bold rounded-full p-2 cursor-pointer'>
-                    <h1>10<span>s</span></h1>
+                <div  onClick={()=>restTimeButton('one')} className='bg-slate-400 hover:bg-slate-900 hover:text-white font-bold rounded-full p-2 cursor-pointer'>
+                    <h1><span id='one'>10</span>s</h1>
                 </div>
-                <div className='bg-slate-400 hover:bg-slate-900 hover:text-white font-bold rounded-full p-2 cursor-pointer'>
-                    <h1>20<span>s</span></h1>
+                <div  onClick={()=>restTimeButton('two')} className='bg-slate-400 hover:bg-slate-900 hover:text-white font-bold rounded-full p-2 cursor-pointer'>
+                    <h1><span id='two'>20</span>s</h1>
                 </div>
-                <div className='bg-slate-400 hover:bg-slate-900 hover:text-white font-bold rounded-full p-2 cursor-pointer'>
-                    <h1>30<span>s</span></h1>
+                <div onClick={()=>restTimeButton('three')} className='bg-slate-400 hover:bg-slate-900 hover:text-white font-bold rounded-full p-2 cursor-pointer'>
+                    <h1><span id='three'>30</span>s</h1>
                 </div>
-                <div className='bg-slate-400 hover:bg-slate-900 hover:text-white font-bold rounded-full p-2 cursor-pointer'>
-                    <h1>40<span>s</span></h1>
+                <div onClick={()=>restTimeButton('four')} className='bg-slate-400 hover:bg-slate-900 hover:text-white font-bold rounded-full p-2 cursor-pointer'>
+                    <h1><span id='four'>40</span>s</h1>
                 </div>
             </div>
+
             <h1 className='text-2xl text-slate-900 font-bold mt-5'>Exercise Details</h1>
+
             <div className='mt-5'>
                 <div className='flex p-2 bg-white rounded-lg mb-4'>
                     <h3 className=' bg-white font-bold'>Exercise time <span className='ml-4 text-slate-900'>{requiredTime} <span className='text-slate-300'>second</span></span></h3>
                 </div>
                 <div className='flex p-2 bg-white rounded-lg'>
-                    <h3 className=' bg-white font-bold'>Rest time <span className='ml-12 text-slate-300'>10 second</span></h3>
+                    <h3 className=' bg-white font-bold'>Rest time <span className='ml-12 text-slate-300'><span id='restTime' className='text-slate-900'>0</span> second</span></h3>
                 </div>
             </div>
             <button className='w-60 pt-2 pb-2 m-auto bg-slate-900 text-white font-bold rounded-lg mt-4 '>Activity Completed</button>
